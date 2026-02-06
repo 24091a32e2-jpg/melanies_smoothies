64eb1edf85f7ea29e2c883c62611f6c58a73869c
MUCHUMARRI SAI SANDHYA <24091a32e2@rgmcet.edu.in>
2026-02-06 17:00:28 +0530

    Refactor smoothie app layout and imports
    Removed unnecessary multiline string and adjusted imports.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,8 @@ from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write(
-  """Choose the fruits you want in your custom Smoothie!
-  """
+  "Choose the fruits you want in your custom Smoothie!"
 )
-
-import streamlit as st
 
 name_on_order = st.text_input("Name of smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
@@ -20,7 +17,7 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list=st.multiselect(
     'choose upto 5 ingredients:'
     ,my_dataframe.select(col("FRUIT_NAME")).to_pandas()["FRUIT_NAME"]
-,max)
+,max_selection=5)
 if ingredients_list:
      st.write(ingredients_list)
      st.text(ingredients_list)
